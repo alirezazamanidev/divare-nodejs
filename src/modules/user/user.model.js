7f45631e9e9128ba9da2474d39b9fa53c7339e7e
alireza <alirezazamanidev80@gmail.com>
2023-12-28 07:09:34 -0500

    create user model

---
+++ src/modules/user/user.model.js
@@ -0,0 +1,19 @@
+
+const mongoose=require('mongoose');
+
+const OTPSchema=new mongoose.Schema({
+    code:{type:String,required:false,default:undefined},
+    expireIn:{type:Number,required:false,default:0}
+})
+const userSchema=new mongoose.Schema({
+    fullName: {type: String, required: false},
+    mobile: {type: String, unique: true, required: true},
+    otp: {type: OTPSchema},
+    verifiedMobile: {type: Boolean, default: false, required: true},
+    accessToken: {type: String},
+},{timestamps:true});
+
+
+module.exports={
+    UserModel:mongoose.model('user',userSchema)
+}
