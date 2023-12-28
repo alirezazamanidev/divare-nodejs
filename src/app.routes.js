c0fee8f8007340da9ba3e91b2705ec1a7beac9c7
alireza <alirezazamanidev80@gmail.com>
2023-12-28 16:07:58 -0500

    Add create post page

--- src/app.routes.js
+++ src/app.routes.js
@@ -1,6 +1,7 @@
 const {AuthRoutes} = require('./modules/auth/auth.routes');
 const { CategoryRoutes } = require('./modules/category/category.routes');
 const { OptionRoutes } = require('./modules/option/option.routes');
+const { PostRouter } = require('./modules/post/post.routes');
 const { UserRoutes } = require('./modules/user/user.routes');
 const router=require('express').Router();
 
@@ -9,6 +10,7 @@ router.use('/auth',AuthRoutes);
 router.use('/user',UserRoutes);
 router.use('/category',CategoryRoutes);
 router.use('/option',OptionRoutes);
+router.use('/post',PostRouter);
 module.exports={
     MainRoutes:router
 }
---
+++ src/modules/post/post.routes.js
@@ -0,0 +1,14 @@
+const {Router} = require("express");
+const {PostController} = require("./post.controller");
+
+const Authorization = require("../../common/guard/authorization.guard");
+
+const router = Router();
+router.get("/create", Authorization, PostController.createPostPage);
+// router.post("/create", Authorization, upload.array("images", 10), postController.create);
+// router.get("/my", Authorization, postController.findMyPosts);/
+// router.delete("/delete/:id", Authorization, postController.rem/ove);
+// router.get("/:id", postController.showPost);/
+module.exports = {
+    PostRouter: router
+};
