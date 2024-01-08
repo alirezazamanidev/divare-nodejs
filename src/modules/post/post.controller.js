8ef23947a2309ea1140c5d8f8508f4e41e643faf
alireza <alirezazamanidev80@gmail.com>
2024-01-08 15:19:58 -0500

    Add multer file for upload images

--- src/modules/post/post.controller.js
+++ src/modules/post/post.controller.js
@@ -2,51 +2,53 @@ const autoBind = require("auto-bind");
 const { PostService } = require("./post.service");
 const { CategoryModel } = require("../category/category.model");
 class PostController {
+  #service;
+  constructor() {
+    autoBind(this);
+    this.#service = PostService;
+  }
 
-    #service;
-    constructor(){
-        autoBind(this);
-        this.#service=PostService;
+  async createPostPage(req, res, next) {
+    try {
+      let { slug } = req.query;
+      let showBack = false;
+      let match = { parent: null };
+      let options, category;
+      if (slug) {
+        slug = slug.trim();
+        category = await CategoryModel.findOne({ slug });
+        if (!category) throw new createHttpError.NotFound(PostMessage.NotFound);
+        options = await this.#service.getCategoryOptions(category._id);
+        if (options.length === 0) options = null;
+        showBack = true;
+        match = {
+          parent: category._id,
+        };
+      }
+      const categories = await CategoryModel.aggregate([
+        {
+          $match: match,
+        },
+      ]);
+      res.render("./pages/panel/create-post.ejs", {
+        categories,
+        showBack,
+        category: category?._id.toString(),
+        options,
+      });
+    } catch (error) {
+      next(error);
     }
-
-        async createPostPage (req, res, next) {
-            try {
-                let {slug} = req.query;
-                let showBack = false;
-                let match = {parent: null};
-                let options, category;
-                if (slug) {
-                    slug = slug.trim();
-                    category = await  CategoryModel.findOne({slug});
-                    if (!category) throw new createHttpError.NotFound(PostMessage.NotFound);
-                    options = await this.#service.getCategoryOptions(category._id);
-                    if (options.length === 0) options = null;
-                    showBack = true;
-                    match = {
-                        parent: category._id
-                    };
-                }
-                const categories = await CategoryModel.aggregate([
-                    {
-                        $match: match
-                    }
-                ]);
-                res.render("./pages/panel/create-post.ejs", {
-                    categories,
-                    showBack,
-                    category: category?._id.toString(),
-                    options,
-    
-                });
-            } catch (error) {
-                next(error);
-            }
-        }
-    
-
-   
+  }
+  async create(req,res,next){
+    try {
+        console.log(req.body);
+    } catch (error) {
+        next(error);
+    }
+  }
 }
 
-module.exports={
-    PostController:new PostController()
-}
+module.exports = {
+  PostController: new PostController(),
+};
--- src/modules/post/post.routes.js
+++ src/modules/post/post.routes.js
@@ -5,10 +5,8 @@ const Authorization = require("../../common/guard/authorization.guard");
 
 const router = Router();
 router.get('/create',PostController.createPostPage);
-// router.post("/create", Authorization, upload.array("images", 10), postController.create);
-// router.get("/my", Authorization, postController.findMyPosts);/
-// router.delete("/delete/:id", Authorization, postController.rem/ove);
-// router.get("/:id", postController.showPost);/
+router.post('/create',PostController.create);
+
 module.exports = {
     PostRouter: router
 };
