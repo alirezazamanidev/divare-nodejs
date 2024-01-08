4cf1247dffec2b58cb0946c747ee1143d70b63ba
alireza <alirezazamanidev80@gmail.com>
2024-01-08 05:34:17 -0500

    create option modules

--- src/modules/option/option.routes.js
+++ src/modules/option/option.routes.js
@@ -2,8 +2,13 @@ const { OptionController } = require('./option.controller');
 
 const router=require('express').Router();
 
-router.post('/add',OptionController.create);
-
+router.post("/", OptionController.create)
+router.get("/by-category/:categoryId", OptionController.findByCategoryId)
+router.get("/by-category-slug/:slug", OptionController.findByCategorySlug)
+router.get("/:id", OptionController.findById)
+router.delete("/:id", OptionController.removeById)
+router.get("/", OptionController.find)
+router.put("/:id", OptionController.update)
 module.exports={
     OptionRoutes:router
 }
--- src/modules/option/option.swagger.js
+++ src/modules/option/option.swagger.js
@@ -2,9 +2,8 @@
  * @swagger
  * tags:
  *  name: Option
- *  description: Option Module and Rpoutes
+ *  description: Option Module and Routes
  */
-
 
 /**
  * @swagger
@@ -87,24 +86,6 @@
  */
 /**
  * @swagger
- * /option/add:
- *  post:
- *   summary: create new option for category
- *   tags:
- *     -  Option
- *   requestBody:
- *         content:
- *           application/x-www-form-urlencoded:
- *                schema:
- *                   $ref: '#/components/schemas/CreateOption'
- *   responses:
- *         201:
- *           description: created!
- *                     
- */
-
-/**
- * @swagger
  * /option/{id}:
  *  put:
  *      summary: updated option by id
@@ -121,4 +102,75 @@
  *      responses:
  *          201: 
  *              description: created
+ */
+/**
+ * @swagger
+ * /option/by-category/{categoryId}:
+ *  get:
+ *      summary: get all options of category
+ *      tags:
+ *          -   Option
+ *      parameters:
+ *          -   in: path        
+ *              name: categoryId
+ *              type: string
+ *      responses:
+ *          200: 
+ *              description: successfully
+ */
+/**
+ * @swagger
+ * /option/by-category-slug/{slug}:
+ *  get:
+ *      summary: get all options of category
+ *      tags:
+ *          -   Option
+ *      parameters:
+ *          -   in: path        
+ *              name: slug
+ *              type: string
+ *      responses:
+ *          200: 
+ *              description: successfully
+ */
+/**
+ * @swagger
+ * /option/{id}:
+ *  get:
+ *      summary: get option by id
+ *      tags:
+ *          -   Option
+ *      parameters:
+ *          -   in: path        
+ *              name: id
+ *              type: string
+ *      responses:
+ *          200: 
+ *              description: successfully
+ */
+/**
+ * @swagger
+ * /option:
+ *  get:
+ *      summary: get all options
+ *      tags:
+ *          -   Option
+ *      responses:
+ *          200: 
+ *              description: successfully
+ */
+/**
+ * @swagger
+ * /option/{id}:
+ *  delete:
+ *      summary: delete option by id
+ *      tags:
+ *          -   Option
+ *      parameters:
+ *          -   in: path        
+ *              name: id
+ *              type: string
+ *      responses:
+ *          200: 
+ *              description: deleted successfully
  */
